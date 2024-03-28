fdad490aa44dbfbcaaf60bc58c0a928abd8f2558
NikolayGrishko <nikolay.grishko.2000@gmail.com>
2024-03-28 13:16:07 +0200

    build: calculation desktop without PerfectPixel

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -6,16 +6,33 @@ hamburger.addEventListener('click', function (){
     headerNav.classList.toggle('show')
 })
 
+
+
 $('#first-screen-slider').slick({
     dots: true,
     appendArrows: $('.first-screen-arrows'),
     responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            arrows: false
-          }
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false
         }
+      }
     ],
-     
+
 });
+
+
+
+const fileInput = document.querySelector('#calculation-file');
+const fileName = document.querySelector('#file-name')
+
+fileInput.addEventListener('change', () => {
+   fileName.innerHTML = fileInput.files[0].name;
+});
+
+//    то же самое но в полном виде
+// fileInput.addEventListener('change', function() {
+//   console.log(this.files[0].name);
+//   fileName.innerHTML = this.files[0].name;
+// });
